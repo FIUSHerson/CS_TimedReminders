a4a296b415351623265f08f7096f9c0945766687
Alexander Anthis (Desktop) <anthisalex@gmail.com>
2020-05-01 09:59:22 -0700

    StartupXaml
    Created framework for startup page

---
+++ Pages/Startup.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_TimedReminders.Pages
+{
+    /// <summary>
+    /// Interaction logic for Startup.xaml
+    /// </summary>
+    public partial class Startup : Page
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Remove_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Edit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Start_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
